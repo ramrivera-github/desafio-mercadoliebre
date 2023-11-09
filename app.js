a0081b11e10485425cb35c1a50389ed523f78921
Ramiro Rivera <ramrivera7@gmail.com>
2023-11-09 01:14:24 -0300

    Actualiza Vinculo Logo

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 const express= require('express')
 const app=express();
-const PORT= process.env.PORT || 3001;
+const port= process.env.PORT || 3001;
 
 app.use('/static',express.static(__dirname + '/public'))
 app.get(
-    '/',(req,res)=> res.sendFile(__dirname + '/views/home.html')
-)
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
+    '/',(req,res)=> res.sendFile(__dirname + '/views/home.html'))
+app.get('/login', (req,res)=> res.sendFile(__dirname + '/views/login.html'))
 
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
+app.get('/register', (req,res)=>res.sendFile(__dirname + '/views/register.html'))
 
-app.listen(PORT ,()=>{
-    console.log("Servidor iniciado en el puerto ",PORT)
-});
+app.listen(port,()=>console.log("Servidor corriendo en el puerto ${port}"))
