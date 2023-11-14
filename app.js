9dc093f13016d14007679de3794675afeff7867b
Ramiro Rivera <ramrivera7@gmail.com>
2023-11-14 18:26:13 -0300

    Detalles finales...

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.use('/static',express.static(__dirname + '/public'))
 app.get('/', (req,res)=> res.sendFile(__dirname + '/views/home.html'))
 app.get('/login', (req,res)=> res.sendFile(__dirname + '/views/login.html'))
 app.get('/register', (req,res)=>res.sendFile(__dirname + '/views/register.html'))
-
+app.post('/', (req,res)=> res.sendFile(__dirname + '/views/home.html'))
 app.listen(port,()=>console.log(`Servidor corriendo en el puerto ${port}`))
