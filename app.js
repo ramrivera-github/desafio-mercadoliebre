449fec01291640973efe2aabe568543cdd76907d
Ramiro Rivera <ramrivera7@gmail.com>
2023-11-14 18:03:41 -0300

    Detalles finales

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const app=express();
 const port= process.env.PORT || 3001;
 
 app.use('/static',express.static(__dirname + '/public'))
-app.get(
-    '/',(req,res)=> res.sendFile(__dirname + '/views/home.html'))
+app.get('/', (req,res)=> res.sendFile(__dirname + '/views/home.html'))
 app.get('/login', (req,res)=> res.sendFile(__dirname + '/views/login.html'))
-
 app.get('/register', (req,res)=>res.sendFile(__dirname + '/views/register.html'))
 
 app.listen(port,()=>console.log(`Servidor corriendo en el puerto ${port}`))
